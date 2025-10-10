757f9077a26846dd8d33eb862a12f6bc0435e33f
BK0SS <kosulin.b@mail.ru>
2025-10-09 19:11:42 -0700

    auth-firebase-feature

--- app/src/main/java/com/example/pockettru/Auth.java
+++ app/src/main/java/com/example/pockettru/Auth.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Auth extends AppCompatActivity {
 
@@ -23,18 +27,45 @@ public class Auth extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Check if the user is already logged in
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+            return;
+        }
+
+
         Intent intent = new Intent(this, MainActivity.class);
         Intent registerIntent = new Intent(this, Register.class);
 
         Button loginButton = findViewById(R.id.login_button);
         Button registerButton = findViewById(R.id.goto_register_button);
 
+        EditText emailInput = findViewById(R.id.email_input);
+        EditText passwordInput = findViewById(R.id.password_input);
+
+       // Login button click listener code for logging in
        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(Auth.this, task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Auth.this, "Authentication successful.", Toast.LENGTH_SHORT).show();
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(Auth.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
                 startActivity(intent);
             }
         });
+
+       // Register button click listener code for going to the register page
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
