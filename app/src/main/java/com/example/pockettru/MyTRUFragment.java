5a3af3e6b0159499035a587bc6d25659075a136f
Rodrigo Ramirez Marquez <rodagol@Rodrigos-MacBook-Pro.local>
2025-10-10 17:17:00 -0700

    Web-view added

--- app/src/main/java/com/example/pockettru/MyTRUFragment.java
+++ app/src/main/java/com/example/pockettru/MyTRUFragment.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MyTRUFragment extends Fragment {
+
+    private WebView mytru_webview;
 
     @Nullable
     @Override
@@ -16,4 +22,26 @@ public class MyTRUFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mytru, container, false);
     }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+            super.onViewCreated(view, savedInstanceState);
+
+            mytru_webview = view.findViewById(R.id.mytru_webview);
+            mytru_webview.setWebViewClient(new WebViewClient());
+
+            WebSettings webSettings = mytru_webview.getSettings();
+
+            webSettings.setJavaScriptEnabled(true);
+
+            webSettings.setDomStorageEnabled(true);
+            webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+            webSettings.setDatabaseEnabled(true);
+            webSettings.setAllowFileAccess(true);
+
+            String wolfpackScheduleUrl = "https://mytru.tru.ca";
+            mytru_webview.loadUrl(wolfpackScheduleUrl);
+        }
+
 }
--- app/src/main/java/com/example/pockettru/NewsFragment.java
+++ app/src/main/java/com/example/pockettru/NewsFragment.java
@@ -4,16 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class NewsFragment extends Fragment {
 
+    private WebView omeganews_webview;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        omeganews_webview = view.findViewById(R.id.omeganews_webview);
+        omeganews_webview.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = omeganews_webview.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowFileAccess(true);
+
+        String wolfpackScheduleUrl = "https://theomega.news";
+        omeganews_webview.loadUrl(wolfpackScheduleUrl);
     }
 }
